41481b0c8f22b194f036f348f295027659fb2415
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-07-05 16:02:33 -0300

    feat: html optimization

---
+++ src/hooks.server.ts
@@ -0,0 +1,36 @@
+import { building } from '$app/environment';
+import { minify, type Options } from 'html-minifier';
+
+const CONFIG = {
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	decodeEntities: true,
+	html5: true,
+	minifyCSS: true,
+	minifyJS: true,
+	removeAttributeQuotes: true,
+	removeComments: false,
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true,
+	collapseInlineTagWhitespace: true
+} satisfies Options;
+
+const PRELOAD = ['font', 'css', 'js', 'asset'];
+
+export const handle = async ({ resolve, event }) => {
+	let page = '';
+
+	return await resolve(event, {
+		preload: ({ type }) => PRELOAD.includes(type),
+		transformPageChunk: ({ html, done }) => {
+			page += html;
+
+			if (done) return building ? minify(page, CONFIG) : page;
+		}
+	});
+};
